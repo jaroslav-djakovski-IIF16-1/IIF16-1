84b36af760aa791df4f0d9e1757cdd8c252d7145
Jaroslav <jaroslav.djakovski@stud.vgtu.lt>
2020-03-29 19:12:12 +0300

    Update Program.cs
    Veikiantis nuskaitymas, apskaičiavimas ir išvedimas į ekraną.

--- Lab 3-4/Program.cs
+++ Lab 3-4/Program.cs
@@ -198,18 +198,37 @@ namespace Lab_3_4
 
         public static void file() {
             
-            var studentai = new List<studentas>();
+            List<studentas> studentai = new List<studentas>();
             int counter = 0;
+            int[] paz = new int[10];
             string line;
+            double vid = 0, med = 0;
             StreamReader stud = new StreamReader(@"d:\Studentai.txt");
             while ((line = stud.ReadLine()) != null)
             {
                 string[] words = line.Split(new char [0], StringSplitOptions.RemoveEmptyEntries);
                 if (counter != 0)
                 {
-                    //Console.WriteLine(words[2]);
+                    for (int i = 2; i <= 7; i++) {
+
+                        vid = vid + Convert.ToInt32(words[i]);
+                    }
+                    vid = vid / 6;
+                    vid = (0.3 * vid) + (0.7 * Convert.ToInt32(words[7]));
+
+                    for (int i = 2; i <= 7; i++) {
+
+                        paz[i-2] = Convert.ToInt32(words[i]);
+                    }
+                    Array.Sort(paz);
+
+                    med = (paz[2] + paz [3]) / 2;
+
+                    med = (0.3 * med) + (0.7 * Convert.ToInt32(words[7]));
+
+                    studentai.Add(new studentas{ vardas = words[0], pavarde = words[1], vidurkis = vid, mediana = med });
                     counter++;
-                    
+
                 }
                 else
                     counter++;
@@ -217,7 +236,16 @@ namespace Lab_3_4
             }
             stud.Close();
 
-            Console.WriteLine("work in progress ");
+
+            string s = new String('-', 80);
+            Console.WriteLine(String.Format("{0,-15} {1,-15} {2,-15} {3,25}", "Vardas", "Pavardė", "Galutinis (vid.)", "Galutinis (med.)"));
+            Console.WriteLine(s);
+            foreach (var student in studentai)
+            {
+                Console.WriteLine(String.Format("{0,-15} {1,-25} {2, -25} {3, 0}", student.vardas, student.pavarde, Math.Round(student.vidurkis, 2), student.mediana));
+               // Console.WriteLine(student.vardas + "  " + student.pavarde + "  " + student.vidurkis + "   " + student.mediana);
+            }
+            Console.WriteLine(s);
             Console.ReadLine();
         }
         static void Main(string[] args)
